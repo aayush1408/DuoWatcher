44b7b8b64c53e76352d6cb1eeef5e759aaa444de
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-30 21:19:35 +0530

    youtube react used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import YouTube from 'react-youtube';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.videoNode = React.createRef();
+    this.buttonNode = React.createRef();    
+  }
+  playPause(){
+    console.log(this.videoNode);
+    this.buttonNode.current.innerHTML = 'Pause'; 
+  }
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+ 
     return (
       <div className="App">
-        <iframe width="420" height="315" src="https://www.youtube.com/embed /SWWDUOP9m0o" frameBorder="0" allowFullScreen></iframe>
+        <YouTube
+        videoId="FB97kk5-zGc"
+        opts={opts}
+        onReady={this._onReady}
+        />
+        <div>
+          <button ref={this.buttonNode} onClick = {this.playPause.bind(this)}>Play</button>
+          <input type="range" min="0" max="100" value="0" step="1" /> 
+        </div>
       </div>
+
     );
   }
 }
